47341beaeabe5926497a306a2eff39886a1b2537
TejaswiniVaishnave <55315595+TejaswiniVaishnave@users.noreply.github.com>
2020-11-08 20:07:01 +0530

    Add files via upload

---
+++ SQL/SQL_Activities.sql
@@ -0,0 +1,158 @@
+create database sdet4_tejaswini;
+use sdet4_tejaswini;
+
+CREATE TABLE salesman (
+    salesman_id int primary key,
+    name varchar(32),
+    city varchar(32),
+    commission int
+);
+
+DESCRIBE salesman;
+INSERT INTO salesman
+VALUES (104, 'Sam', 'Hyderabad', 60000);
+SELECT * FROM salesman;
+INSERT INTO salesman VALUES(5001, 'James Hoog', 'New York', 15);
+INSERT INTO salesman VALUES
+    (5002, 'Nail Knite', 'Paris', 13),
+    (5005, 'Pit Alex', 'London', 11), 
+    (5006, 'McLyon', 'Paris', 14), 
+    (5007, 'Paul Adam', 'Rome', 13),
+    (5003, 'Lauson Hen', 'San Jose', 12);
+  
+SELECT salesman_id, city FROM salesman;
+
+
+SELECT * FROM salesman WHERE city='Bangalore';
+
+
+SELECT salesman_id, commission FROM salesman WHERE name='Sam';
+
+ALTER TABLE salesman ADD grade int;
+
+UPDATE salesman SET grade=100 WHERE salesman_id='101';
+
+UPDATE salesman SET grade=200 WHERE salesman_id='5007';
+
+UPDATE salesman SET grade=300 WHERE salesman_id='5001';
+
+UPDATE salesman SET name='Pierre' WHERE salesman_id='5006';
+
+create table orders(
+    order_no int primary key, purchase_amount float, order_date date,
+    customer_id int, salesman_id int);
+
+
+insert into orders values
+(70001, 150.5, '2012-10-05', 3005, 5002), (70009, 270.65, '2012-09-10', 3001, 5005),
+(70002, 65.26, '2012-10-05', 3002, 5001), (70004, 110.5, '2012-08-17', 3009, 5003),
+(70007, 948.5, '2012-09-10', 3005, 5002), (70005, 2400.6, '2012-07-27', 3007, 5001),
+(70008, 5760, '2012-08-15', 3002, 5001), (70010, 1983.43, '2012-10-10', 3004, 5006),
+(70003, 2480.4, '2012-10-10', 3009, 5003), (70012, 250.45, '2012-06-27', 3008, 5002),
+(70011, 75.29, '2012-08-17', 3003, 5007), (70013, 3045.6, '2012-04-25', 3002, 5001);
+
+
+select distinct salesman_id from orders;
+
+select order_no, order_date from orders order by order_date;
+
+
+select order_no, purchase_amount from orders order by purchase_amount DESC;
+
+
+select * from orders where purchase_amount < 500;
+
+
+select * from orders where purchase_amount between 1000 and 2000;
+
+-- Write an SQL statement to find the total purchase amount of all orders.
+select SUM(purchase_amount) AS "Total" from orders;
+
+-- Write an SQL statement to find the average purchase amount of all orders.
+select AVG(purchase_amount) AS "Average" from orders;
+
+-- Write an SQL statement to get the maximum purchase amount of all the orders.
+select MAX(purchase_amount) AS "Max value" from orders;
+
+-- Write an SQL statement to get the minimum purchase amount of all the orders.
+select MIN(purchase_amount) AS "Min value" from orders;
+
+-- Write an SQL statement to find the number of salesmen listed in the table.
+select COUNT(distinct salesman_id) AS "Count" from orders;
+-- Write an SQL statement to find the highest purchase amount ordered by the each customer with their ID and highest purchase amount.
+SELECT customer_id, MAX(purchase_amount) AS "Max Amount" FROM orders GROUP BY customer_id;
+
+-- Write an SQL statement to find the highest purchase amount on '2012-08-17' for each salesman with their ID.
+SELECT salesman_id, order_date, MAX(purchase_amount) AS "Max Amount" FROM orders 
+WHERE order_date='2012-08-17' GROUP BY salesman_id, order_date;
+
+-- Write an SQL statement to find the highest purchase amount with their ID and order date, for only those customers who have a higher purchase amount within the list 2030, 3450, 5760, and 6000.
+SELECT customer_id, order_date, MAX(purchase_amount) AS "Max Amount" FROM orders
+GROUP BY customer_id, order_date
+HAVING MAX(purchase_amount) IN(2030, 3450, 5760, 6000);
+
+-- Create the customers table
+create table customers (
+    customer_id int primary key, customer_name varchar(32),
+    city varchar(20), grade int, salesman_id int);
+
+-- Insert values into it
+insert into customers values 
+(3002, 'Nick Rimando', 'New York', 100, 5001), (3007, 'Brad Davis', 'New York', 200, 5001),
+(3005, 'Graham Zusi', 'California', 200, 5002), (3008, 'Julian Green', 'London', 300, 5002),
+(3004, 'Fabian Johnson', 'Paris', 300, 5006), (3009, 'Geoff Cameron', 'Berlin', 100, 5003),
+(3003, 'Jozy Altidor', 'Moscow', 200, 5007), (3001, 'Brad Guzan', 'London', 300, 5005);
+    
+    SELECT * From customers;
+-- Write an SQL statement to know which salesman are working for which customer.
+SELECT a.customer_name AS "Customer Name", a.city, b.name AS "Salesman", b.commission FROM customers a 
+INNER JOIN salesman b ON a.salesman_id=b.salesman_id;
+
+-- Write an SQL statement to make a list in ascending order for the customer who holds a grade less than 300 and works either through a salesman
+SELECT a.customer_name, a.city, a.grade, b.name AS "Salesman", b.city FROM customers a 
+LEFT OUTER JOIN salesman b ON a.salesman_id=b.salesman_id WHERE a.grade<300 
+ORDER BY a.customer_id;
+
+-- Write an SQL statement to find the list of customers who appointed a salesman for their jobs who gets a commission from the company is more than 12%.
+SELECT a.customer_name AS "Customer Name", a.city, b.name AS "Salesman", b.commission FROM customers a 
+INNER JOIN salesman b ON a.salesman_id=b.salesman_id 
+WHERE b.commission>12;
+
+-- Write an SQL statement to find the details of a order i.e. order number, order date, amount of order, which customer gives the order and which salesman works for that customer and commission rate he gets for an order.
+SELECT a.order_no, a.order_date, a.purchase_amount, b.customer_name AS "Customer Name", b.grade, c.name AS "Salesman", c.commission FROM orders a 
+INNER JOIN customers b ON a.customer_id=b.customer_id 
+INNER JOIN salesman c ON a.salesman_id=c.salesman_id;
+
+-- Write a query to find all the orders issued against the salesman who may works for customer whose id is 3007.
+SELECT * FROM orders
+WHERE salesman_id=(SELECT DISTINCT salesman_id FROM orders WHERE customer_id=3007);
+
+-- Write a query to find all orders attributed to a salesman in New York.
+SELECT * FROM orders
+WHERE salesman_id IN (SELECT salesman_id FROM salesman WHERE city='New York');
+
+-- Write a query to count the customers with grades above New York's average.
+SELECT grade, COUNT(*) FROM customers
+GROUP BY grade HAVING grade>(SELECT AVG(grade) FROM customers WHERE city='New York');
+
+-- Write a query to extract the data from the orders table for those salesman who earned the maximum commission
+SELECT order_no, purchase_amount, order_date, salesman_id FROM orders
+WHERE salesman_id IN( SELECT salesman_id FROM salesman
+WHERE commission=( SELECT MAX(commission) FROM salesman));
+
+-- Write a query that produces the name and number of each salesman and each customer with more than one current order. Put the results in alphabetical order
+SELECT customer_id, customer_name FROM customers a
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.customer_id = b.customer_id)
+UNION
+SELECT salesman_id, name FROM salesman a
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id)
+ORDER BY customer_name;
+
+-- Write a query to make a report of which salesman produce the largest and smallest orders on each date.
+SELECT a.salesman_id, name, order_no, 'highest on', order_date FROM salesman a, orders b
+WHERE a.salesman_id=b.salesman_id
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date)
+UNION
+SELECT a.salesman_id, name, order_no, 'lowest on', order_date FROM salesman a, orders b
+WHERE a.salesman_id=b.salesman_id
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
